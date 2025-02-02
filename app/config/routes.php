ce4277f3337aed7dd0000222a5d729028e186439
Sylvio3365 <sylvioandri@gmail.com>
2025-02-02 11:40:48 +0300

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,8 +4,6 @@ use app\controllers\ApiExampleController;
 use app\controllers\WelcomeController;
 use flight\Engine;
 use flight\net\Router;
-use Flight;
-
 /** 
  * @var Router $router 
  * @var Engine $app
